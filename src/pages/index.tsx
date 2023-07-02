f718f2a6c99368cde5105047cf057117c4a89f26
s1f10210254 <iniad@DESKTOP-LC3J6L9.localdomain>
2023-07-02 18:30:06 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,11 +14,54 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
+  const directions = [
+    //上から時計回り
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+  ];
+
+  //自分の色があるかどうか探す関数
+  const checkFlipCells = (x: number, y: number, direction: number[]) => {
+    const [dx, dy] = direction;
+    let tempX = x + dx;
+    let tempY = y + dy;
+    const flipCells = [];
+
+    while (board[tempY][tempX] !== undefined) {
+      if (board[tempY][tempX] === 0) {
+        break;
+      } else if (board[tempY][tempX] === turnColor) {
+        return flipCells;
+      } else {
+        flipCells.push([tempY], tempX);
+      }
+      tempX += dx;
+      tempY += dy;
+    }
+    return [];
+  };
+
+  //色を裏返す関数
+  const flipCells = (cells: number[][]) => {
+    for (const cell of cells) {
+      const [y, x] = cell;
+      newBoard[y][x] = turnColor;
+    }
+  };
+
   const onClick = (x: number, y: number) => {
     console.log(x, y);
 
     //2次元配列をコピーして新しいものをつくって変えてもOK！
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
+
+    //隣が相手の色だったら置ける処理
 
     //下が相手の色だったら置ける(上)
     if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
